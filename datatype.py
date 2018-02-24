ddb09c447a610df2ae98a3be6d9065b0ea296f15
anay <anay.latiwal@gmail.com>
2018-02-23 22:00:52 -0800

    updated

---
+++ datatype.py
@@ -0,0 +1,16 @@
+a = 5
+print(a, "is of type", type(a))
+
+a = 2.0
+print(a, "is of type", type(a))
+
+a = 1+2j
+print(a, "is complex number?", isinstance(1+2j,complex))
+
+a = {5,2,3,1,4}
+
+# printing set variable
+print("a = ", a)
+
+# data type of variable a
+print(type(a))
---
+++ python_output.py
@@ -0,0 +1,4 @@
+print("This is the output of the screen")
+
+a= 5
+print('The value of a is ',a)
